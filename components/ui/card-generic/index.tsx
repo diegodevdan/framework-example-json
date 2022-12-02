5af578b6aa6ec0b29b51f516fc8c2654b29f6626
Diego Herrera <diego.herrera@tekchoice.com>
2022-12-02 06:56:48 -0600

    Wip styles generic card

--- components/ui/card-generic/index.tsx
+++ components/ui/card-generic/index.tsx
@@ -2,6 +2,7 @@ import React, {FC, useEffect, useState} from 'react';
 import {IconAlertCircle, IconCash, IconCircleCheck, IconCircleX, IconThumbUp, IconUser} from "@tabler/icons";
 import {Text} from "@mantine/core";
 import {openConfirmModal} from "@mantine/modals";
+import styles from './styles.module.css';
 
 interface TextProps {
     icon: string,
@@ -32,12 +33,12 @@ export const CardGeneric = (data: CardGenericProps) => {
 
     const getIcon = (icon:string) => {
         switch (icon) {
-            case 'caution': return <IconAlertCircle />
-            case 'user': return <IconUser />
-            case 'thumb': return <IconThumbUp />
-            case 'cash': return <IconCash />
-            case 'success': return <IconCircleCheck />
-            case 'fail': return <IconCircleX />
+            case 'caution': return <IconAlertCircle className={styles.icon} color={'red'} />
+            case 'user': return <IconUser className={styles.icon} color={'cyan'}/>
+            case 'thumb': return <IconThumbUp className={styles.icon} color='brown'/>
+            case 'cash': return <IconCash className={styles.icon} color={'green'}/>
+            case 'success': return <IconCircleCheck className={styles.icon} color={'green'} size={20}/>
+            case 'fail': return <IconCircleX className={styles.icon} color={'red'} size={20}/>
             default: break;
         }
     }
@@ -53,13 +54,16 @@ export const CardGeneric = (data: CardGenericProps) => {
 
     const RenderData = () => {
         return (
-            <div onClick={openModal}>
+            <div
+                className={styles.contCard}
+                onClick={openModal}
+            >
                 <h1>{getIcon(Data.icon)} {Data && Data.title}</h1>
 
                 <div>
                     {
                         Data.data.text.map(el => (
-                            <p>{getIcon(el.icon)} {el.text}</p>
+                            <p className={styles.text}>{getIcon(el.icon)} {el.text}</p>
                         ))
                     }
                 </div>
@@ -68,7 +72,7 @@ export const CardGeneric = (data: CardGenericProps) => {
     }
 
     return (
-        <div>
+        <div className={styles.main}>
             {
                 Data && <RenderData />
             }
